39ad72075757bb4e61b5886623935046a1b4eacd
MohamedElyamani <58939494+MohamedElyamani@users.noreply.github.com>
2023-08-06 18:17:06 +0200

    this is devfolio

---
+++ js/myJs.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.type', {
+
+    strings: ['Front End Developer', 
+              'Front End Designer',
+              'E-Learning Developer'],
+              typeSpeed: 60,
+              backSpeed: 60,
+              loop: true,
+  });
